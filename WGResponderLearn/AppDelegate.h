501e2815b9a4b6f6e4983c7ff7f6c7573379f56d
wanggang <978159546@qq.com>
2018-07-27 10:07:09 +0800

    Initial Commit

---
+++ WGResponderLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGResponderLearn
+//
+//  Created by wanggang on 2018/7/27.
+//  Copyright © 2018年 wanggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WGResponderLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGResponderLearn
+//
+//  Created by wanggang on 2018/7/27.
+//  Copyright © 2018年 wanggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
